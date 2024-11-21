3f7988c67dd05d3d42f78f7c691d2e23cefb4e10
matheus <pachecx35@gmail.com>
2024-11-21 20:12:42 -0300

    add fontes

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import './style.css'
 
 const NavBar = () => {
   return (
-    <nav>NavBar</nav>
+    <div>
+    <nav>
+      <ol>
+        <li>
+          <a>
+            home
+          </a>
+        </li>
+        <li>
+          <a>
+          Sobre Nós
+          </a>
+        </li>
+        <li>
+          <a>
+          Benefícios
+          </a>
+        </li>
+        <li>
+          <a>
+          Certificação
+          </a>
+        </li>
+        <li>
+          <a>
+          Auditores
+          </a>
+        </li>
+        <li>
+          <a>
+          Checklist
+          </a>
+        </li>
+      </ol>
+      </nav>
+    </div>
   )
 }
 
